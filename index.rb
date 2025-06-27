51a65ffe4a9a7f081eeb3afa622c3a54de1ff453
AngryHickory <angryhickory33@gmail.com>
2025-06-27 07:58:25 -0700

    Add subtraction of base ascii value and char_ascii
    Added a line which will subtract the larger ASCII value with the current character,
    this will create an index between 0-25, excluding all the characters not included in the alphabet.

--- index.rb
+++ index.rb
@@ -6,9 +6,11 @@ def caesar_cypher(string, shift)
       if char =~ /[A-Z]/
         char_ascii = char.ord
         base_ascii_for_uppercase = 'A'.ord
+        char_ascii - base_ascii_for_uppercase
       elsif char =~ /[a-z]/
         char_ascii = char.ord
         base_ascii_for_lowercase = 'a'.ord
+        char_ascii - base_ascii_for_lowercase
       end
     else
       result << char
